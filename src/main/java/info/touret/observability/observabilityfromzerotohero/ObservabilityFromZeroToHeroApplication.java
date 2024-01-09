8e6ed1704f2ff1538171dcbe93db36b29541e191
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2024-01-09 11:25:29 +0100

    feat: Initialisation

---
+++ src/main/java/info/touret/observability/observabilityfromzerotohero/ObservabilityFromZeroToHeroApplication.java
@@ -0,0 +1,13 @@
+package info.touret.observability.observabilityfromzerotohero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityFromZeroToHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityFromZeroToHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/touret/observability/observabilityfromzerotohero/ObservabilityFromZeroToHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package info.touret.observability.observabilityfromzerotohero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityFromZeroToHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
